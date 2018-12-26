fc3f7c43e6a6277692a1473e7449825847417167
TriantafyllosFamprikatzis <alkisfab@gmail.com>
2018-12-26 22:41:30 +0200

    JS updates, added a check letter function and event listener to the buttons

--- app.js
+++ app.js
@@ -1,48 +1,71 @@
 //Variables
-const qwerty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
-const buttonReset = document.querySelector('.btn__reset');
-const overlay = document.getElementById('overlay');
-const missed = 0;
+const QWERTY = document.getElementById('qwerty');
+const PHRASE = document.getElementById('phrase');
+const BUTTON_RESET = document.querySelector('.btn__reset');
+const OVERLAY = document.getElementById('overlay');
+const LETTERS = document.getElementsByClassName('letter');
+const MISSED = 0;
 
-//Hide the overlay with a click
-buttonReset.addEventListener('click', () => {
- overlay.style.display = 'none';
+//Hide the overlay by pressing Strat Game 
+BUTTON_RESET.addEventListener('click', () => {
+ OVERLAY.style.display = 'none';
 });
 
 //All phrases in an Array
-let phrases = [
+const PHRASES = [
 'A fool and his money are soon parted',
 'A friend in need is a friend indeed',
 'Beauty is only skin deep',
-'Caught red-handed',
+'Caught redhanded',
 'Close quarters',
 'Doom and gloom',
 'Enough is enough'
 ];
 
-// Function to get a random phrase and split it to arrays
+//Get a random phrase and split that phrase into array of letters
 function getRandomPhraseAsArray (arr) {
    let randomPhrase = Math.floor(Math.random() * (arr.length));
-   return arr[randomPhrase].split("");
+   return arr[randomPhrase].split('');
    }
 
-//Function to display a phrase
+/*loop through the array of letters and, 
+create a new li if matches a letter and not a space */
  function addPhraseToDisplay (arr) {
-      for (i = 0; i < arr.length; i += 1) {
-         let letters = /^[A-Za-z]+$/;
-         let match = arr[i].match(letters);
+      for (let i = 0; i < arr.length; i += 1) {
          let li = document.createElement('li');
          let ul = document.getElementsByTagName('ul')[0];
          li.textContent = arr[i];
          ul.appendChild(li); 
-           if (match)   {
+           if (arr[i].match(/^[A-Za-z]+$/))   {
             li.className = 'letter';
+         }
+         else {
+            li.className = 'space';
          }
       }
    };
 
-   //Call the functions
-   const phraseArray = getRandomPhraseAsArray(phrases);
-   addPhraseToDisplay(phraseArray);
+//Call the two functions
+const PHRASE_ARRAY = getRandomPhraseAsArray(PHRASES);
+addPhraseToDisplay(PHRASE_ARRAY);
 
+
+//Function to check if the button clicked match the letter
+ function checkLetter (btn)  {
+   let guessed = null;
+   for(let i = 0; i < LETTERS.length; i += 1){
+      if (btn.target.textContent === LETTERS[i].textContent.toLowerCase()) {
+         guessed = true;
+         LETTERS[i].className += ' show, chosen';
+         btn.target.disabled = true;
+      }
+   }return guessed;
+ }
+
+ QWERTY.addEventListener('click', (event) => {
+   letterFound = checkLetter(event);
+ });
+
+
+
+ 
