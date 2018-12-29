0a94db025bd3d8133a7f05d619e3e6a3cb032f00
TriantafyllosFamprikatzis <alkisfab@gmail.com>
2018-12-29 19:44:31 +0200

    General Updates

--- app.js
+++ app.js
@@ -4,40 +4,41 @@ const phrase = document.getElementById('phrase');
 const buttonReset = document.querySelector('.btn__reset');
 const overlay = document.getElementById('overlay');
 const letters = document.getElementsByClassName('letter');
-const show = document.getElementsByClassName('show');
-const title = document.querySelector('h2');
+const shownLetters = document.getElementsByClassName('show');
+const overlayTitle = document.querySelector('h2');
+const heart = document.getElementsByTagName('img');
 let missed = 0;
+let reset = false;
 
-
-//All phrases in an Array
+//All phrases
 const phrases = [
-   'A fish out of water','A nest of vipers','Abracadabra','All Greek to me',
-   'As white as snow','Back to basics','Battle royal','Below the belt',
-   'Bells and whistles','Bite the bullet','Bite the dust','Bitter end',
-   'Blue moon','Break a leg','Bucket list','Carpe diem',
-   'Charmed life','Cold feet','Cut to the chase','Close quarters',
-   'Doom and gloom','Enough is enough','Easy as pie','End of story',
-   'Fair and square','Fall from grace','Fall on your sword','Fashion victim',
-   'Feeding frenzy','Fellow traveller','Fiddlesticks','Finders Keepers',
-   'Go berserk','Hands down','Harvest moon','Hat trick',
-   'Hem and haw','Hocus pocus','Hold your horses','Household words',
-   'Inside out','Keep at bay','Know your onions','La dolce vita',
-   'Last but not least','Less is more','Lie low','Loose cannon',
-   'Love is blind','Make my day','Mend fences','Night owl',
-   'On the bubble','On your tod','One for the road','Out of sight',
-   'Pass the buck','Pennies from heaven','Pie in the sky','Pig in a poke',
-   'Pitch black','Point to point','Prime time','Pull up stakes',
-   'Road less travelled','Rise and shine','Run of the mill','Shake a leg',
-   'Shiver my timbers','Shoot through','Silence is golden','Silver bullet',
-   'Silver lining','Sleep tight','Sleeveless errand','Smart casual',
-   'Speak of the Devil','Stick in the mud','Such is life','Surf and turf',
-   'Swing for you','Take a back seat','Take the upper hand','Talk to the hand',
-   'Tell me about it','The bitter end','The camera cannot lie','The last straw',
-   'The more the merrier','Thorn in the flesh','Thumbs up','Tie the knot',
-   'Tit for tat','Tooth and nail','Top notch','Touch and go',
-   'Trick or treat','Tuckered out','Turn a blind eye','Under the thumb',
-   'Upside down','Vice versa','Weasel words','When pigs fly',
-   'Whistle and flute','White as snow'
+   'A fish out of water', 'A nest of vipers', 'Abracadabra', 'All Greek to me',
+   'As white as snow', 'Back to basics', 'Battle royal', 'Below the belt',
+   'Bells and whistles', 'Bite the bullet', 'Bite the dust', 'Bitter end',
+   'Blue moon', 'Break a leg', 'Bucket list', 'Carpe diem',
+   'Charmed life', 'Cold feet', 'Cut to the chase', 'Close quarters',
+   'Doom and gloom', 'Enough is enough', 'Easy as pie', 'End of story',
+   'Fair and square', 'Fall from grace', 'Fall on your sword', 'Fashion victim',
+   'Feeding frenzy', 'Fellow traveller', 'Fiddlesticks', 'Finders Keepers',
+   'Go berserk', 'Hands down', 'Harvest moon', 'Hat trick',
+   'Hem and haw', 'Hocus pocus', 'Hold your horses', 'Household words',
+   'Inside out', 'Keep at bay', 'Know your onions', 'La dolce vita',
+   'Last but not least', 'Less is more', 'Lie low', 'Loose cannon',
+   'Love is blind', 'Make my day', 'Mend fences', 'Night owl',
+   'On the bubble', 'On your tod', 'One for the road', 'Out of sight',
+   'Pass the buck', 'Pennies from heaven', 'Pie in the sky', 'Pig in a poke',
+   'Pitch black', 'Point to point', 'Prime time', 'Pull up stakes',
+   'Road less travelled', 'Rise and shine', 'Run of the mill', 'Shake a leg',
+   'Shiver my timbers', 'Shoot through', 'Silence is golden', 'Silver bullet',
+   'Silver lining', 'Sleep tight', 'Sleeveless errand', 'Smart casual',
+   'Speak of the Devil', 'Stick in the mud', 'Such is life', 'Surf and turf',
+   'Swing for you', 'Take a back seat', 'Take the upper hand', 'Talk to the hand',
+   'Tell me about it', 'The bitter end', 'The camera cannot lie', 'The last straw',
+   'The more the merrier', 'Thorn in the flesh', 'Thumbs up', 'Tie the knot',
+   'Tit for tat', 'Tooth and nail', 'Top notch', 'Touch and go',
+   'Trick or treat', 'Tuckered out', 'Turn a blind eye', 'Under the thumb',
+   'Upside down', 'Vice versa', 'Weasel words', 'When pigs fly',
+   'Whistle and flute', 'White as snow'
 ];
 
 //Get a random phrase and split that phrase into array of letters
@@ -62,23 +63,10 @@ function addPhraseToDisplay(arr) {
       }
    }
 }
-//   let regex = /^[A-Za-z]+$/.value;
-//   if (arr[i].textContent === )
-
-
-//   let regex = /^[A-Za-z]+$/;
-//   let value = regex.value;
-//   if (arr[i].textContent === )
-
-
-//    let regex =  /^[A-Za-z]+$/;
-//    if(! regex)
-
 
 //Call the two functions
 const phraseArray = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phraseArray);
-
 
 //Function to check if the button clicked match the letter
 function checkLetter(btn) {
@@ -86,101 +74,76 @@ function checkLetter(btn) {
    for (let i = 0; i < letters.length; i += 1) {
       if (btn.target.textContent === letters[i].textContent.toLowerCase()) {
          guessed = true;
-         letters[i].className += ' show chosen';
-         btn.target.disabled = true;
+         letters[i].className += ' show';
       }
    } return guessed;
 }
 
-
 function checkWin() {
-         if (letters.length === show.length) {
-            overlay.style.display = '';
-            overlay.className = 'win';
-            title.innerHTML = 'win';
-            buttonReset.textContent = 'Reset Game';
-         } else {
-            //the number of misses is equal to or greater than 5, show the overlay screen with the “lose” 
-            //class and appropriate text.
-         }
-      }
+   if (letters.length === shownLetters.length) {
+      overlay.style.display = '';
+      overlay.className = 'win';
+      overlayTitle.innerHTML = 'Awesome You Found It!!!';
+      buttonReset.textContent = 'I Want More';
+      reset = true;
+   } else if (missed === 5) {
+      overlay.style.display = '';
+      overlay.className = 'lose';
+      overlayTitle.innerHTML = 'Game Over';
+      buttonReset.textContent = 'Start Again';
+      reset = true;
+   }
+}
 
-
-//Hide the overlay by pressing Start Game 
+/*Hide the overlay by pressing Start Game, 
+then if won or lost the game reset everything*/
 buttonReset.addEventListener('click', () => {
    overlay.style.display = 'none';
+   if (reset === true) {
+      missed = 0;
+      for (i = 0; i < heart.length; i += 1) {
+         heart[i].src = 'images/liveHeart.png';
+      }
+      for(i = 0; i < letters.length; i += 1){
+         letters[i].className = 'letter';
+      }
+      buttons = document.getElementsByTagName('button');
+      for(i = 0; i < buttons.length; i += 1){
+         buttons[i].className = '';
+         buttons[i].disabled = false;
+      }
+      addPhraseToDisplay(phraseArray);
+   }
 });
 
-
-// qwerty.addEventListener("click", event => {
-//   letterFound = checkLetter(event);
-
-//   let img = document.getElementsByTagName('img')[0];
- 
-//   if (letterFound === null && missed < 5) {
-//     missed += 1;
-//     qwerty.style.backgroundColor = "red";
-//     img.src = "images/lostHeart.png";
-//    //  for(i = 0; i < img.length; i += 1){
-//    //    img[i].src = "images/lostHeart.png";
-//    //  }
-//   }
-//   checkWin();
-// });
-
-
-//if(guess == 5)
-
-
-// qwerty.addEventListener("click", event => {
-//    letterFound = checkLetter(event);
-
-//    let img = document.getElementsByTagName('img');
-//    for (i = 0; i < img.length; i += 1) {
-//       if (letterFound === null && missed < 5) {
-//          missed += 1;
-//          qwerty.style.backgroundColor = "red";
-//          img[i].src = "images/lostHeart.png";
-//       }
-//    }
-//    checkWin();
-// });
-
-
-
-
-
-
-
-
-qwerty.addEventListener("click", event => {
+qwerty.addEventListener("click", (event) => {
    letterFound = checkLetter(event);
- 
-   const heart = document.getElementsByTagName('img');
-  
-   if (letterFound === null && missed < 5) {
-     missed += 1;
-     if(missed === 1){
-     heart[0].src = "images/lostHeart.png";
-     event.target.disabled = true;
-     }
-     else if(missed === 2) {
-      heart[1].src = "images/lostHeart.png";
+   if (event.target.tagName === 'BUTTON'){
+      event.target.className = 'chosen';
       event.target.disabled = true;
-     }
-     else if (missed === 3) {
-      heart[2].src = "images/lostHeart.png";
-      event.target.disabled = true;
-     }
-     else if (missed === 4) {
-      heart[3].src = "images/lostHeart.png";
-      event.target.disabled = true;
-     }
-     else if (missed === 5) {
-      heart[4].src = "images/lostHeart.png";
-      event.target.disabled = true;
-     }
+      if (letterFound === null && missed < 5) {
+         missed += 1;
+         if (missed === 1) {
+            heart[0].src = 'images/lostHeart.png';
+            event.target.disabled = true;
+         }
+         else if (missed === 2) {
+            heart[1].src = 'images/lostHeart.png';
+            event.target.disabled = true;
+         }
+         else if (missed === 3) {
+            heart[2].src = 'images/lostHeart.png';
+            event.target.disabled = true;
+         }
+         else if (missed === 4) {
+            heart[3].src = 'images/lostHeart.png';
+            event.target.disabled = true;
+         }
+         else if (missed === 5) {
+            heart[4].src = 'images/lostHeart.png';
+            event.target.disabled = true;
+         }
+      }
    }
-
    checkWin();
- });
+});
